3ac97f77f3a9064990c94ed90fe26ad91ccc41f9
Adam <Red3lmo@gmail.com>
2015-04-21 20:46:05 +0100

    fdg

---
+++ Website/Assets/JS/custom.js
@@ -0,0 +1,10 @@
+//$(function swoop() {
+//	alert("test");
+//	});
+
+$(function swoop() {
+    $('#button1').click(function () {
+        $('imageOne').attr('src','http://vignette2.wikia.nocookie.net/mlp-gameloft/images/6/6a/Fluttershy_Vector.png/revision/latest?cb=20141214215316' )
+    })
+        
+	});
---
+++ Website/Assets/JS/tipuesearch/tipuesearch_content.js
@@ -0,0 +1,8 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Index", "text": "Click <a href='index.html'>here</a> to go back to the Home Page", "tags": "Home Index", "url": "index.html"},
+     {"title": "Tipue Search, a site search engine jQuery plugin", "text": "Tipue Search is a site search engine jQuery plugin. It's free, open source, responsive and fast. Tipue Search only needs a browser that supports jQuery. It doesn't need MySQL or similar. In Static mode it doesn't even need a web server.", "tags": "JavaScript", "url": "http://www.tipue.com/search"}
+]};
+
+
+
